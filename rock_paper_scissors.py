48c4c564ed2122f60a4773d2103256f051410678
Maria <mariahojan@gmail.com>
2017-02-21 11:13:26 -0800

    second draft

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -28,62 +28,73 @@ users_choice = raw_input("Enter your choice:")
 users_choice = users_choice.upper()
 
 #If the user input is same as the random number, display the tie
-
-if users_choice == computers_choice:
+while True:
 	
-	print users_name + " ,you chose ",users_choice
-	print "Computer chose ",computers_choice
-	print "Tie!"
+	if users_choice == computers_choice:
+	
+		print users_name + " ,you chose ",users_choice
+		print "Computer chose ",computers_choice
+		print "Tie!"
 
 #If the user input is rock and the computer choice is scissors, display that the user won
-
-elif users_choice == 'R' and computers_choice == 'S':
-    print users_name + " ,you entered Rock. Computer had Scissor. Rock smashes Scissors. You win!"
-
+	
+	elif users_choice == 'R' and computers_choice == 'S':
+		print users_name + " ,you entered Rock. Computer had Scissor. Rock smashes Scissors. You win!"
 #storing the score
-	users_score = users_score + 1
+		users_score = users_score + 1
 
 
 #If the user input is rock and the computer choice is paper, display that the computer won
 
-elif users_choice == 'R' and computers_choice == 'P':
-    print users_name + " ,you entered Rock. Computer had Paper. Paper covers Rock. You lose!"
+	elif users_choice == 'R' and computers_choice == 'P':
+		print users_name + " ,you entered Rock. Computer had Paper. Paper covers Rock. You lose!"
 
 #storing the score
 
-	computers_score = computers_score + 1
+		computers_score = computers_score + 1
 
 #If the user input is paper and the computer choice is scissors, display that the user won
 
-elif users_choice == 'P' and computers_choice == 'S':
-    print users_name + " ,you entered Paper. Computer had Scissors. Scissors cut Paper. You lose!"
+	elif users_choice == 'P' and computers_choice == 'S':
+		print users_name + " ,you entered Paper. Computer had Scissors. Scissors cut Paper. You lose!"
 
 #storing the score
 
-	computers_score = computers_score + 1
+		computers_score = computers_score + 1
 
 
-elif users_choice == 'P' and computers_choice == 'R':
-    print users_name + " ,you entered Paper. Computer had Rock. Paper covers Rock. You win!"
+	elif users_choice == 'P' and computers_choice == 'R':
+		print users_name + " ,you entered Paper. Computer had Rock. Paper covers Rock. You win!"
+
+#storing the score
+		users_score = users_score + 1
+
+	elif users_choice == 'S' and computers_choice == 'P':
+		print users_name + " ,you entered Scissors. Computer had Paper. Scissors cut Paper. You win!"
 
 #storing the score
 
-	users_score = users_score + 1
+		users_score = users_score + 1
 
-elif users_choice == 'S' and computers_choice == 'P':
-    print users_name + " ,you entered Scissors. Computer had Paper. Scissors cut Paper. You win!"
-
+	elif users_choice == 'S' and computers_choice == 'R':
+		print users_name + " ,you entered Scissors. Computer had Rock. Rock smashes Scissors. You lose!"
 #storing the score
-
-	users_score = users_score + 1
-
-elif users_choice == 'S' and computers_choice == 'R':
-    print users_name + " ,you entered Scissors. Computer had Rock. Rock smashes Scissors. You lose!"
+		computers_score = computers_score + 1
+	else:
+		print "Invalid input! You have not entered rock(R), paper(P) or scissors(P), try again."
 
 
-#storing the score
+#winner
 
-	computers_score = computers_score + 1
+	if users_score == 5:
+			 #Print computer wins the game
+		print "Congratulations " + users_name + " ,you've reached 5 points! You win!"
+		break
+    #Provide proper conditions for elif in case user getting 5 points:
+	else:
+		#Congratulate the user on winning along with their name
+		print users_name + " ,Computer's reached 5 points! You lose!"
+		break
 
 
 
